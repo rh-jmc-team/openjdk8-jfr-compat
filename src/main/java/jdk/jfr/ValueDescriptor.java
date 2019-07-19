b70053848ee79ea74e6e685fa0b9e311424572d7
Jessye Coleman-Shapiro <jescolem@redhat.com>
2019-07-19 16:49:11 -0400

    added missing constructor in ValueDescriptor.java and added new test script

--- src/main/java/jdk/jfr/ValueDescriptor.java
+++ src/main/java/jdk/jfr/ValueDescriptor.java
@@ -1,8 +1,13 @@
 package jdk.jfr;
 
+import java.util.List;
+
 public final class ValueDescriptor {
 	
     public ValueDescriptor(Class<?> type, String name) {
+    }
+    
+    public ValueDescriptor(Class<?> type, String nam, List<AnnotationElement> annotations) {	
     }
 
 }
---
+++ src/test/java/AnnotationElementTest.java
@@ -0,0 +1,29 @@
+import jdk.jfr.Event;
+import jdk.jfr.AnnotationElement;
+import jdk.jfr.ValueDescriptor;
+import jdk.jfr.EventFactory;
+import jdk.jfr.Description;
+import jdk.jfr.Name;
+import jdk.jfr.Label;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class AnnotationElementTest{
+	public static void main (String args[]) {
+		List<AnnotationElement> typeAnnotations = new ArrayList<>();
+   		typeAnnotations.add(new AnnotationElement(Name.class, "com.example.HelloWorld"));
+   		typeAnnotations.add(new AnnotationElement(Label.class, "Hello World"));
+	   	typeAnnotations.add(new AnnotationElement(Description.class, "Helps programmer getting started"));
+
+   		List<AnnotationElement> fieldAnnotations = new ArrayList<>();
+		fieldAnnotations.add(new AnnotationElement(Label.class, "Message"));
+
+  	 	List<ValueDescriptor> fields = new ArrayList<>();
+   		fields.add(new ValueDescriptor(String.class, "message", fieldAnnotations));
+
+   		EventFactory f = EventFactory.create(typeAnnotations, fields);
+   		Event event = f.newEvent();
+  	  	event.commit();
+	}
+}
