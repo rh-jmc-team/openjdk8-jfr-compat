9e3aa91a770ea8681ff6b0c4f427e7af8920c3c6
Jessye Coleman-Shapiro <jescolem@redhat.com>
2019-07-26 10:41:35 -0400

    Removed  '* Copyright (c) 2019, Red Hat Inc. All rights reserved.' from file copywrite headers

--- src/main/java/jdk/jfr/Name.java
+++ src/main/java/jdk/jfr/Name.java
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2016, 2018, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2019, Red Hat Inc. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +22,6 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-
 package jdk.jfr;
 
 import java.lang.annotation.ElementType;
